0a083292603bfb9b65c2e81bb3820108ead076b2
Ahmad <ahmadbazaib7@gmail.com>
2025-03-09 15:18:41 +0530

    small edit to add space between index and value

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ const displayMovements = function (movements) {
     const html = `<div class="movements__row">
           <div class="movements__type movements__type--${type}">${
       i + 1
-    }${type}</div>
+    } ${type}</div>
           <div class="movements__value">${mov}</div>
         </div>`;
 
