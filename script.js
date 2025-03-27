20e621992b0ce7647fc05c6c93a0e7c358836864
Ahmad <ahmadbazaib7@gmail.com>
2025-03-27 18:25:23 +0530

    calculate and display balance for account1

--- script.js
+++ script.js
@@ -79,6 +79,12 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
@@ -323,4 +329,11 @@ console.log(balance);
 let balance2 = 0;
 for (const mov of movements) balance2 += mov;
 console.log(balance2);
+
+// Maximum Value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+});
+console.log(max);
 */
