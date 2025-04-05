01ffdcc8209a33b5764bd3cdf5ebbdbd6b3f1b6a
Ahmad <ahmadbazaib7@gmail.com>
2025-04-05 19:20:38 +0530

    add sorting_feature in the movements_transaction

--- script.js
+++ script.js
@@ -61,9 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `<div class="movements__row">
@@ -211,6 +214,13 @@ btnClose.addEventListener('click', function (e) {
     inputCloseUsername.value = inputClosePin.value = '';
     labelWelcome.textContent = `Log in to get started`;
   }
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -781,6 +791,7 @@ fetch? HINT: Use the "Math.max" method along with the ... operator.
 // console.log(fetchWeight);
 // console.log(heaviestFetchBreed);
 
+/*
 ////////////////////////////////
 // SORTING ARRAYS
 
@@ -811,3 +822,4 @@ console.log(movements);
 // });
 movements.sort((a, b) => b - a);
 console.log(movements);
+*/
