8ab1f00a708f6cdab538f20858cb37987ea6e419
Ahmad <ahmadbazaib7@gmail.com>
2025-04-09 12:42:08 +0530

    creating and filling arrays

--- script.js
+++ script.js
@@ -830,34 +830,69 @@ console.log(movements);
 
 ////////////////////////////////
 // ARRAY GROUPING
-console.log(movements);
+// console.log(movements);
 
-const groupedMovements = Object.groupBy(movements, movement =>
-  movement > 0 ? 'deposits' : 'withdrawals'
-);
-console.log(groupedMovements);
+// const groupedMovements = Object.groupBy(movements, movement =>
+//   movement > 0 ? 'deposits' : 'withdrawals'
+// );
+// console.log(groupedMovements);
 
-const groupedByActivity = Object.groupBy(accounts, account => {
-  const movementCount = account.movements.length;
+// const groupedByActivity = Object.groupBy(accounts, account => {
+//   const movementCount = account.movements.length;
 
-  if (movementCount >= 8) {
-    return 'very active';
-  } else if (movementCount >= 4) {
-    return 'active';
-  } else if (movementCount >= 1) {
-    return 'moderate';
-  } else {
-    return 'inactive';
-  }
+//   if (movementCount >= 8) {
+//     return 'very active';
+//   } else if (movementCount >= 4) {
+//     return 'active';
+//   } else if (movementCount >= 1) {
+//     return 'moderate';
+//   } else {
+//     return 'inactive';
+//   }
+// });
+
+// console.log(groupedByActivity);
+
+// // const groupedAccounts = Object.groupBy(accounts, account => account.type);
+// const groupedAccounts = Object.groupBy(accounts, ({ type }) => type);
+// /*
+// Putting ({type}) in parentheses tells JavaScript:
+// “Hey, this is a function parameter, and I’m using destructuring!
+// */
+// console.log(groupedAccounts);
+
+////////////////////////////////
+// MORE WAYS OF CREATING AND FILLING ARRAYS
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty + Fill
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+x.fill(1, 3, 5);
+x.fill(1);
+console.log(x);
+
+arr.fill(23, 2, 5);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+  // but we would have to do apping differently
 });
-
-console.log(groupedByActivity);
-
-// const groupedAccounts = Object.groupBy(accounts, account => account.type);
-const groupedAccounts = Object.groupBy(accounts, ({ type }) => type);
-/*
-Putting ({type}) in parentheses tells JavaScript:
-
-“Hey, this is a function parameter, and I’m using destructuring!
-*/
-console.log(groupedAccounts);
