41d139a8933df8cc5b474d173215ee9296227c0d
Ahmad <ahmadbazaib7@gmail.com>
2025-04-09 12:52:05 +0530

    Non-Destructive array methods

--- script.js
+++ script.js
@@ -864,35 +864,50 @@ console.log(movements);
 ////////////////////////////////
 // MORE WAYS OF CREATING AND FILLING ARRAYS
 
-const arr = [1, 2, 3, 4, 5, 6, 7];
-console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(new Array(1, 2, 3, 4, 5, 6, 7));
 
-// Empty + Fill
-const x = new Array(7);
-console.log(x);
-// console.log(x.map(() => 5));
-x.fill(1, 3, 5);
-x.fill(1);
-console.log(x);
+// // Empty + Fill
+// const x = new Array(7);
+// console.log(x);
+// // console.log(x.map(() => 5));
+// x.fill(1, 3, 5);
+// x.fill(1);
+// console.log(x);
 
-arr.fill(23, 2, 5);
-console.log(arr);
+// arr.fill(23, 2, 5);
+// console.log(arr);
 
-// Array.from
-const y = Array.from({ length: 7 }, () => 1);
-console.log(y);
+// // Array.from
+// const y = Array.from({ length: 7 }, () => 1);
+// console.log(y);
 
-const z = Array.from({ length: 7 }, (_, i) => i + 1);
-console.log(z);
+// const z = Array.from({ length: 7 }, (_, i) => i + 1);
+// console.log(z);
 
-labelBalance.addEventListener('click', function () {
-  const movementsUI = Array.from(
-    document.querySelectorAll('.movements__value'),
-    el => Number(el.textContent.replace('€', ''))
-  );
+// labelBalance.addEventListener('click', function () {
+//   const movementsUI = Array.from(
+//     document.querySelectorAll('.movements__value'),
+//     el => Number(el.textContent.replace('€', ''))
+//   );
 
-  console.log(movementsUI);
+//   console.log(movementsUI);
 
-  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
-  // but we would have to do apping differently
-});
+//   const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+//   // but we would have to do apping differently
+// });
+
+////////////////////////////////
+// NON-DESTRUCTIVE ALTERNATIVES: TOREVERSED, TOSORTED, TOSLICED, WITH
+
+// console.log(movements);
+// // const reversedMov = movements.reverse();
+// // const reversedMov = movements.slice().reverse();
+// const reversedMov = movements.toReversed();
+// console.log(reversedMov);
+
+// // toSorted(sort), toSpliced(splice)
+// // movements[1] = 2000;
+// const newMovements = movements.with(1, 2000);
+// console.log(newMovements);
+// console.log(movements);
