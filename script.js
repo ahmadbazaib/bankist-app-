8deee4b9e682e5cb184fd0e28e1ad97bbf49bdc4
Ahmad <ahmadbazaib7@gmail.com>
2025-04-13 17:18:28 +0530

    timers: setTimeout and setInterval

--- script.js
+++ script.js
@@ -284,14 +284,16 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add Movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add Movement
+      currentAccount.movements.push(amount);
 
-    // Add loan Date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      // Add loan Date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
 
   inputLoanAmount.value = '';
@@ -1427,4 +1429,25 @@ console.log('USA:', new Intl.NumberFormat('en-US', options).format(num));
 console.log('Germany:', new Intl.NumberFormat('de-DE', options).format(num));
 console.log('Syria:', new Intl.NumberFormat('ar-SY', options).format(num));
 console.log('India:', new Intl.NumberFormat('hi-IN', options).format(num));
+
+////////////////////////////////
+// TIMERS: SETTIMEOUT & SETINTERVAL
+
+// setTimeout
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}!`),
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+
+// setInterval(function () {
+//   const now = new Date();
+//   console.log(now);
+// }, 1000);
 */
